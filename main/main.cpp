eaa2183bc394440f379c65bdef6fe5d2904854c5
iceCream <142316765+Angelo11199@users.noreply.github.com>
2023-11-22 07:52:30 -0500

    reverted

--- main/main.cpp
+++ main/main.cpp
@@ -3,6 +3,12 @@
 #include "../include/snippets.h"  //ALWAYS ADD THIS
 using namespace std;
 int main() {
-
+    printLn("Hello World! on the first line || ");
+    print("this is on a same line");
+    print("");  // to print a newline
+    int num = getNum("Enter a number:");
+    print("You entered: " + to_string(num));
+    string str = getStr("Enter a string:");
+    print("You entered: " + str);
     return 0;
 }
